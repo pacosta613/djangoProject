b74f7dafe93926c95bd1c646f73d3c5e813dd8a9
pacosta613 <pedro.acosta613@gmail.com>
2017-04-26 14:44:27 -0400

    updated settings, created app, added south, enabled django admin

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -124,6 +124,9 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'django.contrib.admin',
+    'myapp',
+    'south'
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
--- djangoProject/djangoProject/urls.py
+++ djangoProject/djangoProject/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -14,4 +16,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+
 )
